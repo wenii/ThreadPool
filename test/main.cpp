1013f183f04b66bbe2de864b49b9cbc8cda87a4f
wenii <727420175@qq.com>
2022-04-12 01:05:16 +0800

    first commit.

---
+++ test/main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <iostream>
+#include <atomic>
+#include <random>
+#include "ThreadPool.h"
+
+
+static int GetRandomTime(int intval)
+{
+    return random() % intval;
+}
+int main()
+{
+    srand(time(nullptr));
+    std::atomic<int> count(0);
+    {
+        threadpool::ThreadPool pool(1, 4, 6);
+        int n = 10000;
+        while(n--){
+            pool.AddTask([&](void* arg){
+                ++count;
+                std::this_thread::sleep_for(std::chrono::microseconds(GetRandomTime(1000)));
+            }, nullptr);
+            std::this_thread::sleep_for(std::chrono::microseconds(GetRandomTime(100)));
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(7));
+
+        n = 10000;
+        while(n--){
+            pool.AddTask([&](void* arg){
+                ++count;
+                std::this_thread::sleep_for(std::chrono::microseconds(GetRandomTime(1000)));
+            }, nullptr);
+            std::this_thread::sleep_for(std::chrono::microseconds(GetRandomTime(100)));
+        }
+    }
+    printf("count:%d\n", count.load());
+    return 0;
+}
